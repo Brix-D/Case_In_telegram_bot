395ae584bff171d2e7212825eee9d14d01efff64
Brix-D <Shlyakhta4@gmail.com>
2021-03-22 09:16:25 +0400

    refactored Calendar

--- deadlines/handlers.py
+++ deadlines/handlers.py
@@ -1,17 +1,19 @@
 from aiogram.types import Message
 from aiogram.dispatcher.filters import Text
 
+from apiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
 from app import dispatcher
 from main.handlers import general_menu
+from main.config import client_secret_calendar, calendar_id
 from main.helpers.menu import back_to_menu, deadlines_menu
 from main.helpers.smiles import create_smile
+from deadlines.helpers import Calendar
 
 import httplib2
 import datetime
 import time
-from main.config import client_secret_calendar, calendar_id
-from apiclient import discovery
-from oauth2client.service_account import ServiceAccountCredentials
 
 
 @dispatcher.message_handler(Text("События" + create_smile("\\ud83d\\uddd3")), state="*")
@@ -31,31 +33,31 @@ async def show_calendar(message: Message):
         :param message:
         :return:
         """
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(client_secret_calendar,
-                                                                   'https://www.googleapis.com/auth/calendar')
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
+    calendar = Calendar()
+
     now = datetime.datetime.utcnow().isoformat() + 'Z'
-    now_1week = round(time.time()) + 604800
-    now_1week = datetime.datetime.fromtimestamp(now_1week).isoformat() + 'Z'
-    events_result = service.events().list(
-        calendarId=calendar_id, timeMin=now, timeMax=now_1week, maxResults=100, singleEvents=True,
-        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-    if not events:
+    next_week = round(time.time()) + 604800
+    next_week = datetime.datetime.fromtimestamp(next_week).isoformat() + 'Z'
+
+    events_list = calendar.get_events_for_range(now, next_week)
+
+    if not events_list:
         await message.answer(text='Нет событий на ближайшую неделю')
     else:
-        msg = '<b>События на ближайшую неделю:</b>\n'
-        for event in events:
+        message = '<b>События на ближайшую неделю:</b>\n'
+        for event in events_list:
             if not event['description']:
-                ev_desc = 'Нет описания'
+                event_description = 'Нет описания'
             else:
-                ev_desc = event['description']
-            ev_title = event['summary']
-            cal_link = '<a href="%s">Подробнее...</a>' % event['htmlLink']
-            ev_start = event['start'].get('dateTime')
-            msg = msg + '%s\n%s\n%s\n%s\n\n' % (ev_title, ev_start, ev_desc, cal_link)
-        await message.answer(msg, parse_mode='HTML')
+                event_description = event['description']
+            event_title = event['summary']
+            full_calendar_link = '<a href="%s">Подробнее...</a>' % event['htmlLink']
+            event_start = event['start'].get('dateTime')
+
+            # message = message + '%s\n%s\n%s\n%s\n\n' % (event_title, event_start, event_description, full_calendar_link)
+
+            message = message + f"{event_title}\n{event_start}\n{event_description}\n\n\n{full_calendar_link}"
+        await message.answer(message, parse_mode='HTML')
         await general_menu(message)
 
 
---
+++ deadlines/helpers/Calendar.py
@@ -0,0 +1,25 @@
+import httplib2
+from apiclient import discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+from main.config import client_secret_calendar, calendar_id
+
+
+class Calendar:
+
+    def __init__(self):
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(client_secret_calendar,
+                                                                       'https://www.googleapis.com/auth/calendar')
+        http = credentials.authorize(httplib2.Http())
+        self.connection = discovery.build('calendar', 'v3', http=http)
+
+    def get_events_for_range(self, date_start, date_end):
+        events_result = self.connection.events().list(
+            calendarId=calendar_id, timeMin=now, timeMax=now_1week, maxResults=100, singleEvents=True,
+            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+        return events
+
+    def create_event(self):
+        # To Do
+        pass
